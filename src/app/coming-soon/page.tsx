9cea0d1fc02fc4a4eea363d17ca894fafadf74aa
zhang <zhang_zhikai@outlook.com>
2026-04-29 13:03:38 +0000

    fix: typo

--- src/app/coming-soon/page.tsx
+++ src/app/coming-soon/page.tsx
@@ -1,12 +1,12 @@
 export const metadata = {
-  title: "Comming soon",
+  title: "Coming soon",
 };
 
 export default function NotFound() {
   return (
     <div className="min-h-[75vh] bg-white text-black font-sans px-6 py-36">
       <div className="mx-8 md:mx-40 my-20">
-        <h1 className="text-4xl font-bold mb-2">Comming soon</h1>
+        <h1 className="text-4xl font-bold mb-2">Coming soon</h1>
         <h2 className="text-2xl font-semibold text-gray-600">
           We are almost ready to launch!
         </h2>
