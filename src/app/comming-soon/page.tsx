fc883891e8d620805fd529d233d00e79f40fc7c4
zhang <zhang_zhikai@outlook.com>
2026-04-29 10:53:00 +0000

    fix: not-found mobile style

--- src/app/comming-soon/page.tsx
+++ src/app/comming-soon/page.tsx
@@ -5,7 +5,7 @@ export const metadata = {
 export default function NotFound() {
   return (
     <div className="min-h-[75vh] bg-white text-black font-sans px-6 py-36">
-      <div className="mx-40 my-20">
+      <div className="mx-8 md:mx-40 my-20">
         <h1 className="text-4xl font-bold mb-2">Comming soon</h1>
         <h2 className="text-2xl font-semibold text-gray-600">
           We are almost ready to launch!
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ export const metadata = {
 export default function NotFound() {
   return (
     <div className="min-h-[75vh] bg-white text-black font-sans px-6 py-36">
-      <div className="mx-40 my-20">
+      <div className="mx-8 md:mx-40 my-20">
         <h1 className="text-4xl font-bold mb-2">404</h1>
         <h2 className="text-2xl font-semibold text-gray-600">
           Page not found, please check the URL
